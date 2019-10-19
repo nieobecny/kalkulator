c28145cad10bc4f7abef796bcd11720f3679083b
nieobecny <tomasz.daniel.kaminski@gmail.com>
2019-10-19 09:13:30 +0200

    add menu to my application

--- kalkulatormilion/Program.cs
+++ kalkulatormilion/Program.cs
@@ -10,6 +10,14 @@ namespace kalkulatormilion
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                Console.WriteLine("0. Wyjscie");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
